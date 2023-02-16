69564a65418ffbcee8bbb37140c9e0a2e75de557
原桐 <yt.vertigo0927@gmail.com>
2023-02-16 10:40:42 +0800

    删除main逻辑

--- main.go
+++ main.go
@@ -2,15 +2,9 @@ package main
 
 import (
 	log "github.com/sirupsen/logrus"
-	"os/exec"
 )
 
 func main() {
-	//find, _ := exec.LookPath("php")
-	//log.Info(find)
-	temp := exec.Command("php", "-v")
-	out, _ := temp.Output()
-	log.Info(string(out))
 	log.Info("demon main")
 
 }
